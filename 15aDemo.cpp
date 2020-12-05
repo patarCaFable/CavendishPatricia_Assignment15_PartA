2eacd8b3fa3f05552a0ac9f5d4269140bef3b0e4
Patricia <p.c@s.edu>
2020-12-05 17:27:18 -0500

    First commit

---
+++ 15aDemo.cpp
@@ -0,0 +1,26 @@
+/*Patricia Cavendish
+CIS 1202 502
+DUE 12/6/2020
+12/5/2020*/
+
+
+#include <iostream>
+using namespace std;
+char character(char start, int offset);
+
+int main() {
+	char start;
+	int offset;
+
+	character(start, offset);
+}
+
+char character(char start, int offset)
+{
+	if (!start.isAlpha()) //
+	{
+
+	}
+	else if ()
+	return 0;
+}
