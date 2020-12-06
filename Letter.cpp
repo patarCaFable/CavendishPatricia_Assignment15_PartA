b434768ef93124c65e2e740c83672b6c2f8de2a1
Patricia Cavendish <patricia.m.cavendish@gmail.com>
2020-12-06 18:24:52 -0500

    12/6/2020 6:23EST by PMC
    Letter member class functions broken down into smaller more useful functions.
    
    renamed some variables and member functions.

--- Letter.cpp
+++ Letter.cpp
@@ -13,59 +13,81 @@ int main() {
 	int o;
 	//create a Letter object
 	Letter letter1;
-	//get the starting point and offset
+
+	//get the target character's starting point
 	cout << "Enter the starting letter: ";
 	cin >> s;
 	cout << "Enter the offset: ";
 	cin >> o;
-	//store these values in the Letter object
-	try{
-		letter1.setCharacter(s, o);
-		cout << "New letter: " << letter1.getCharacter();
-	}
-	catch (Letter::invalidCharacterException) {
-		cout << "Error: An invalid character was entered.\n";
-	}
-	catch (Letter::invalidRangeException) {
-		cout << "Error: An invalid range was enetered.\n";
-	}
-	catch (Letter::invalidCaseException) {
-		cout << "Error: Case-to-case transition not allowed.\n";
-	}
+	//store the start value in the Letter object
+		try {
+			letter1.setStart(s);
+			letter1.setOffset(o);
+			letter1.setTarget(s, o);
+			cout << "New letter: " << letter1.getTarget();
+		}
+		catch (Letter::InvalidCharacterException) {
+			cout << "ERROR! Character entered was not a letter.";
+		}
+		catch (Letter::InvalidRangeException) {
+			cout << "ERROR! Invalid range.";
+		}
+		catch (Letter::InvalidCaseException) {
+			cout << "ERROR! Case-to-case transition not allowed!";
+		}
 	cout << endl << endl;
 	system("PAUSE");
 	return 0;
 }
 
-//SET CHARACTER																	
-//Letter class constructor.														
-Letter::Letter()
+//SET START
+//Letter class mutator function. Accepts one char parameter. Sets the value of start. Potentially throws exception.
+void Letter::setStart(char s)
 {
-	start = 'nu';
-	target = 'nu';
-	offset = 0;
+	if (!isalpha(s)) {								//if start is not a letter of the alphabet
+		throw InvalidCharacterException();
+	}
+	start = s;
 }
 
-//SET CHARACTER																	 
-//Letter class mutator function. Accepts two parameters. Sets the value of target.
-void Letter::setCharacter(char start, int offset)
+//SET OFFSET
+//Letter class mutator function. Accepts one int parameter. Sets the value of offset. Potentially throws exception.
+void Letter::setOffset(int o)
 {
-	if (!isalpha(start)) {						//if start is not a letter of the alphabet
-		throw invalidCharacterException();
+	if (!isalpha(getStart() + o)) {				//if the target passes outside of the acceptable range
+		throw InvalidRangeException();
 	}
-	else if (!isalpha(start + offset)) {				//if the target passes outside of the acceptable range
-		throw invalidRangeException();
-	}
-	else if (isupper(start) && islower(start + offset) || 
-		islower(start) && isupper(start + offset)) {		//if upper-lower case transition or vice-versa  occurs
-		throw invalidCaseException();
-	}
-	target = start + offset;
+	offset = o;
 }
 
-//SET CHARACTER																	
-//Letter class accessor function. Accepts no parameters. Returns the value of target.
-char Letter::getCharacter()
+//SET TARGET																 
+//Letter class mutator function. Accepts two parameters of char and int type. Sets the value of target. Potentially throws exception.
+void Letter::setTarget(char s, int o)
+{
+	if (isupper(getStart()) && islower(getStart() + o) || islower(getStart()) && isupper(getStart() + o))
+	{											//if case transition occurs
+		throw InvalidCaseException();
+	}
+	target = getStart() + getOffset();
+}
+
+//GET TARGET																
+//Letter class accessor function. Returns the value of target.
+char Letter::getTarget()
 {
 	return target;
+}
+
+//GET START
+//Letter class accessor function. Returns the value of start.
+char Letter::getStart()
+{
+	return start;
+}
+
+//GET OFFSET
+//Letter class accessor function. Returns the value of offset.
+int Letter::getOffset() 
+{
+	return offset;
 }
--- Letter.h
+++ Letter.h
@@ -5,14 +5,16 @@ private:
 	char start, target;
 	int offset;
 public:
-	Letter();
-	void setCharacter(char start, int offset);
-	char getCharacter();
+	void setStart(char start);
+	char getStart();
+	void setOffset(int offset);
+	int getOffset();
+	void setTarget(char start, int offset);
+	char getTarget();
 	//exception class for a non-alphabetical character
-	class invalidCharacterException {};
+	class InvalidCharacterException {};
 	//exception class for an invalid range
-	class invalidRangeException {};
+	class InvalidRangeException {};
 	//exception class for case-to-case transition
-	class invalidCaseException {};
+	class InvalidCaseException {};
 };
-
